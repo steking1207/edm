42bcaf94217b3205b8f476c32202b52d4cf0c396
steking1207 <steking1207@gmail.com>
2018-10-19 17:13:27 +0800

    1

---
+++ assets/js/custom.js
@@ -0,0 +1,24 @@
+$('.dropdown-menu').click(function(event){
+    event.stopPropagation();
+});
+
+//$('.slick-nav').slick({
+//    infinite: false,
+//});
+
+
+// $('.slider-for').slick({
+//  slidesToShow: 1,
+//  slidesToScroll: 1,
+//  arrows: false,
+//  fade: true,
+//  asNavFor: '.slider-nav'
+//});
+//$('.slider-nav').slick({
+//  slidesToShow: 3,
+//  slidesToScroll: 1,
+//  asNavFor: '.slider-for',
+//  dots: true,
+//  centerMode: true,
+//  focusOnSelect: true
+//});
