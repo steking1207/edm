7230061cef28102fa6e7d750565c366f7cf49b03
steking1207 <steking1207@gmail.com>
2018-10-19 17:03:12 +0800

    181019

---
+++ view/incl/header.php
@@ -0,0 +1,24 @@
+<header class="header">
+    <div class="inner">
+        <div class="header__top">
+            <div class="header__column">
+                <button class="burger">
+                    <span class="burger__line"></span>
+                    <span class="burger__line"></span>
+                    <span class="burger__line"></span>
+                </button>
+                <a class="header__brand" href="/"></a>
+            </div>
+            <div class="header__column">
+                <a class="u-show-line-md" href="/about">關於我們</a>
+                <a class="btn -clr-primary" href="/appointment">立即諮詢</a>
+            </div>
+        </div>
+    </div>
+</header>
+
+<div class="mobileMenu">
+    <div class="mobileMenu__content">
+        <a href="/about">關於我們</a>
+    </div>
+</div>
---
+++ view/incl/setting.php
@@ -0,0 +1,34 @@
+<?php
+echo "\t\t"; getUpdatedHtmlTag(['css/main.css', 'css/main.css']);
+echo "\t\t"; getUpdatedHtmlTag(['css/jquery.scrollbar.css', 'css/jquery.scrollbar.css']);
+echo "\t\t"; getUpdatedHtmlTag(['css/owl.carousel.css', 'css/owl.carousel.css']);
+echo "\n";
+?>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125424865-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-125424865-1');
+</script>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W53SKVJ');</script>
+<!-- End Google Tag Manager -->
+
+<!--Start of Zendesk Chat Script-->
+<script type="text/javascript">
+    window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+        d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+        $.src="https://v2.zopim.com/?5zmiNh9U76cobw1jbgLfQckMULwbngHS";z.t=+new Date;$.
+            type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zendesk Chat Script-->
