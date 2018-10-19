5431d639a7767d7b7fb64ef246d4cb9488c1ec48
steking1207 <steking1207@gmail.com>
2018-10-19 16:59:53 +0800

    181019

---
+++ backup/view/incl/loader.php
@@ -0,0 +1,8 @@
+<div class="loader">
+    <div class="initial-load-animation">
+        <div class="linkedin-image"></div>
+        <div class="loading-bar">
+            <div class="blue-bar"></div>
+        </div>
+    </div>
+</div>
